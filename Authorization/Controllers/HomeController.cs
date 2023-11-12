67c6871ecc9730160a5be9563447ef15e3fcd52c
mirzamujtaba22 <mirzamujtaba22@gmail.com>
2023-11-12 08:58:48 +0500

    Add project files.

---
+++ Authorization/Controllers/HomeController.cs
@@ -0,0 +1,81 @@
+﻿using Authorization.Models;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace Authorization.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+        //Apply Authorization here
+        [Authorize]
+        public IActionResult Secured()
+        {
+            return View();
+        }
+        [HttpGet]
+        public IActionResult Login(string ReturnUrl)
+        {
+            ViewData["returnedUrl"]= ReturnUrl;
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+        [Authorize(Roles ="Admin,Dev")]
+        public IActionResult Secret()
+        {
+            return View();
+        }
+        [HttpPost("Home/Login")]
+        public IActionResult Verify(string username,string password,string ReturnedUrl)
+        {
+            if(username=="sam" || password == "sam")
+            {
+                List<Claim> claims = new List<Claim>();
+                claims.Add(new Claim(ClaimTypes.NameIdentifier, username));
+                claims.Add(new Claim(ClaimTypes.Name, username));
+                claims.Add(new Claim(ClaimTypes.Name, "Admin"));
+                //claims.Add(new Claim(ClaimTypes.Name, "Dev"));
+
+
+                //Claim
+                ClaimsIdentity claimsIdentity = new ClaimsIdentity(claims,CookieAuthenticationDefaults.AuthenticationScheme);
+                //ClaimIdentity
+                ClaimsPrincipal claimsPrincipal = new ClaimsPrincipal(claimsIdentity);
+                HttpContext.SignInAsync(claimsPrincipal);
+                //ClaimsPrinciple
+                return Redirect(ReturnedUrl);
+
+            }
+            return BadRequest();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
